fef792736456d9faf879c74682413a5f5c52972c
ginnoon <ginnoon1324@gmail.com>
2024-12-07 23:02:12 +0900

    u

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 window.onload = function () {
-	const scrollableContainer = document.querySelector('.slides');
-	const slides = document.querySelectorAll('.slide');
-	const slideWidth = 100; // 각 슬라이드의 너비 (px 단위)
-	
+	const Slides = document.querySelector('.slides');
 	let isDragging = false;
 	let startX;
-	
-	// 각 슬라이드의 너비 설정
-	slides.forEach(slide => {
-		slide.style.width = `${slideWidth}px`;
-	});
-	
-	scrollableContainer.addEventListener('mousedown', (event) => {
+
+	Slides.addEventListener('mousedown', (event) => {
 		isDragging = true;
 		startX = event.clientX;
 	});
-	
+
 	document.addEventListener('mousemove', (event) => {
 		if (isDragging) {
 			const currentX = event.clientX;
 			const deltaX = currentX - startX;
-			scrollableContainer.scrollLeft -= deltaX;
+			Slides.scrollLeft -= deltaX;
 			startX = currentX;
 		}
 	});
-	
-	document.addEventListener('mouseup', () => {
-		isDragging = false;
-	});
+
+	document.addEventListener('mouseup', () => isDragging = false);
 }
