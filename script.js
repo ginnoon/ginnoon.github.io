3e3d445e944c4c305dd1ac2f8f492b0da2c5b5b7
ginnoon <ginnoon1324@gmail.com>
2024-12-08 16:54:26 +0900

    u

--- script.js
+++ script.js
@@ -23,4 +23,15 @@ window.onload = function () {
 		e.preventDefault();
 		Slides.scrollBy(e.deltaY, 0);
 	});
+
+	const imglist = [
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/lando-2024-mobile-wallpaper.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/formula-1/2024/schedule/singapore-grand-prix/post-race/singapore-gp-lando-victory-2023-mobile.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/okx-mp4-livery-singapore-mobile-wallpaper.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/2024-monza-lando-wallpaper-mobile.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/miami-wallpaper-mobile.jpg'
+	];
+	document.querySelectorAll('.slide').forEach((element, index) => {
+    element.style.backgroundImage = `url(${imglist[index]})`;
+});
 }
