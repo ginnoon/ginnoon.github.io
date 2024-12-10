6317c9f31a2f66ce3c0e838436868433e218a783
ginnoon <ginnoon1324@gmail.com>
2024-12-10 15:28:13 +0900

    u

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 window.onload = function () {
+	var swiper = new Swiper('.swiper', {
+		direction: 'horizontal',
+		loop: true,
+		// autoplay: {
+		// 	delay: 5000,
+		// 	disableOnInteraction: false,
+		// },
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		spaceBetween: 8,
+		grabCursor : true,
+	});
 
 	const imglist = [
-		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/lando-2024-mobile-wallpaper.jpg',
-		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/formula-1/2024/schedule/singapore-grand-prix/post-race/singapore-gp-lando-victory-2023-mobile.jpg',
-		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/okx-mp4-livery-singapore-mobile-wallpaper.jpg',
-		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/2024-monza-lando-wallpaper-mobile.jpg',
-		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/miami-wallpaper-mobile.jpg'
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/lando-2024-desktop-wallpaper.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/formula-1/2024/schedule/singapore-grand-prix/post-race/singapore-gp-lando-victory-2023-desktop.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/2024-monza-lando-wallpaper-desktop.jpg',
+		'https://mclaren.bloomreach.io/delivery/resources/content/gallery/mclaren-racing/downloads/2024/miami-wallpaper-desktop.jpg'
 	];
-	document.querySelectorAll('.slide').forEach((element, index) => {
+	document.querySelectorAll('.swiper-slide').forEach((element, index) => {
 		element.style.backgroundImage = `url(${imglist[index]})`;
 	});
 }
